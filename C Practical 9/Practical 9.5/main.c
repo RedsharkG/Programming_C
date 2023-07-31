f431b42556dcd12178f51609934bd87c613e2bfe
Shehara Gishan <141002617+RedsharkG@users.noreply.github.com>
2023-07-31 12:34:29 +0530

    Add files via upload

---
+++ C Practical 9/Practical 9.5/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num,re=0;
+    printf("Enter your number: ");
+    scanf("%d",&num);
+
+    do
+    {
+        re=re*10;
+        re=re+num%10;
+        num=num/10;
+
+    }
+    while (num>0);
+    printf("Reverce number: %d",re);
+
+}
